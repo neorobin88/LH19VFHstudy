60b8fa0bbb6adff72564330f6da376f71a1e5cf8
Stefan Hoeche <shoeche@fnal.gov>
2020-06-02 12:03:22 -0500

    added jet radius scan

--- Rivet/MC_HJETSVBF.cc
+++ Rivet/MC_HJETSVBF.cc
@@ -20,8 +20,12 @@ namespace Rivet {
 
       // Projections
       NonPromptFinalState fs(Cuts::abseta < 5);
-      FastJets fj(fs, FastJets::ANTIKT, 0.4);
-      declare(fj, "Jets");
+      for (size_t i = 0; i < DRS.size(); ++i) {
+	FastJets fj(fs, FastJets::ANTIKT, DRS[i]);
+	std::string dr = "_xxx";
+	sprintf(&dr[1], "%0.1f", DRS[i]);
+	declare(fj, "Jets"+dr);
+      }
       PromptFinalState pfs(Cuts::abseta < 5);
       PromptFinalState higgses(Cuts::pid == PID::HIGGS && Cuts::abseta < 5);
       declare(pfs, "PromptFS");
@@ -30,36 +34,43 @@ namespace Rivet {
 
       // Histograms, starting with standard m12, dy12 combinations
       /// @todo Add an extra loop, array layer, and suffix element for the jet radius
+      for (size_t j = 0; j < DRS.size(); ++j) {
+	std::string dr = "_xxx";
+	sprintf(&dr[1], "%0.1f", DRS[j]);
       for (size_t i = 0; i < SELNAMES.size(); ++i) {
-        const string sn = SELNAMES[i];
-        book(_c_xs[i], "cross"+(sn.empty() ? "_full" : sn));
-        book(_h[i]["deltaphi_jj"], "deltaphi_jj"+sn, 6, 0.0, M_PI);
-        book(_h[i]["y_j12_02bin"], "y_j12_02bin"+sn, linspace(20, 0.0, 4.0)+linspace(4, 4.5, 6.5)+linspace(3, 7.0, 10));
-        book(_h[i]["pth_largebin"], "pth_largebin"+sn, linspace(4, 0, 100)+linspace(7, 150, 500)+linspace(2, 600, 1000));
-        book(_h[i]["pth_finebin"], "pth_finebin"+sn, linspace(19, 0, 95)+linspace(9, 100, 190)+linspace(16, 200, 600));
-        book(_h[i]["njets"], "njets"+sn, 5, -0.5, 4.5);
-        book(_h[i]["HT"], "HT"+sn, linspace(10, 0, 1000)+linspace(4, 1200, 2000));
-        book(_h[i]["pthjj_largebin"], "pthjj_largebin"+sn, linspace(4, 0, 100)+linspace(7, 150, 500)+linspace(2, 600, 1000));
-        book(_h[i]["ptjj_largebin"], "ptjj_largebin"+sn, linspace(4, 0, 100)+linspace(7, 150, 500)+linspace(2, 600, 1000));
+        const string sn = dr+SELNAMES[i];
+        book(_c_xs[j][i], "cross"+sn);
+        book(_h[j][i]["deltaphi_jj"], "deltaphi_jj"+sn, 6, 0.0, M_PI);
+        book(_h[j][i]["y_j12_02bin"], "y_j12_02bin"+sn, linspace(20, 0.0, 4.0)+linspace(4, 4.5, 6.5)+linspace(3, 7.0, 10));
+        book(_h[j][i]["pth_largebin"], "pth_largebin"+sn, linspace(4, 0, 100)+linspace(7, 150, 500)+linspace(2, 600, 1000));
+        book(_h[j][i]["pth_finebin"], "pth_finebin"+sn, linspace(19, 0, 95)+linspace(9, 100, 190)+linspace(16, 200, 600));
+        book(_h[j][i]["njets"], "njets"+sn, 5, -0.5, 4.5);
+        book(_h[j][i]["HT"], "HT"+sn, linspace(10, 0, 1000)+linspace(4, 1200, 2000));
+        book(_h[j][i]["pthjj_largebin"], "pthjj_largebin"+sn, linspace(4, 0, 100)+linspace(7, 150, 500)+linspace(2, 600, 1000));
+        book(_h[j][i]["logpthjj_largebin"], "logpthjj_largebin"+sn, linspace(100, 0., 4.));
+        book(_h[j][i]["ptjj_largebin"], "ptjj_largebin"+sn, linspace(4, 0, 100)+linspace(7, 150, 500)+linspace(2, 600, 1000));
       }
       // Now the m12 histograms, with various binnings and dy12 cuts
-      book(_h_mjj["mjj_STXS"], "mjj_STXS", {0.,100.,200.,350.,700.,1000.,1500.,2000.,2500.,3000.});
-      book(_h_mjj["mjj_ATLAS_CONF_029"], "mjj_ATLAS_CONF_029", {0.,160.,500.,1500.});
-      book(_h_mjj["mjj_100GeVbin"], "mjj_100GeVbin", 20, 0, 2000);
-      book(_h_mjj["mjj_100GeVbin_center"], "mjj_100GeVbin_center", 20, 0, 2000);
-      book(_h_mjj["mjj_100GeVbin_middle"], "mjj_100GeVbin_middle", 20, 0, 2000);
-      book(_h_mjj["mjj_100GeVbin_forward"], "mjj_100GeVbin_forward", 20, 0, 2000);
-      book(_h_mjj["mjj_100GeVbin_ATLAS"], "mjj_100GeVbin_ATLAS", 20, 0, 2000);
+      book(_h_mjj[j]["mjj_STXS"], "mjj_STXS", {0.,100.,200.,350.,700.,1000.,1500.,2000.,2500.,3000.});
+      book(_h_mjj[j]["mjj_ATLAS_CONF_029"], "mjj_ATLAS_CONF_029", {0.,160.,500.,1500.});
+      book(_h_mjj[j]["mjj_100GeVbin"], "mjj_100GeVbin", 20, 0, 2000);
+      book(_h_mjj[j]["mjj_100GeVbin_center"], "mjj_100GeVbin_center", 20, 0, 2000);
+      book(_h_mjj[j]["mjj_100GeVbin_middle"], "mjj_100GeVbin_middle", 20, 0, 2000);
+      book(_h_mjj[j]["mjj_100GeVbin_forward"], "mjj_100GeVbin_forward", 20, 0, 2000);
+      book(_h_mjj[j]["mjj_100GeVbin_ATLAS"], "mjj_100GeVbin_ATLAS", 20, 0, 2000);
       // Now the dy12 histograms, with various m12 cuts
-      book(_h_dyjj["deltay_jj"], "deltay_jj", 10, 0, 10);
-      book(_h_dyjj["deltay_jj_light"], "deltay_jj_light", 10, 0, 10);
-      book(_h_dyjj["deltay_jj_heavy"], "deltay_jj_heavy", 10, 0, 10);
+      book(_h_dyjj[j]["deltay_jj"], "deltay_jj", 10, 0, 10);
+      book(_h_dyjj[j]["deltay_jj_light"], "deltay_jj_light", 10, 0, 10);
+      book(_h_dyjj[j]["deltay_jj_heavy"], "deltay_jj_heavy", 10, 0, 10);
+      }
     }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
-
+      for (size_t j = 0; j < DRS.size(); ++j) {
+	std::string dr = "_xxx";
+	sprintf(&dr[1], "%0.1f", DRS[j]);
       // Get Higgs
       const Particles higgses = apply<ParticleFinder>(event, "Higgses").particles();
       if (higgses.empty()) vetoEvent;
@@ -68,7 +79,7 @@ namespace Rivet {
       const double ptH = higgs.pT();
 
       // Get jets and leading dijet system
-      const Jets jets = apply<FastJets>(event, "Jets").jetsByPt(Cuts::pT > 30*GeV && Cuts::absrap < 4.4);
+      const Jets jets = apply<FastJets>(event, "Jets"+dr).jetsByPt(Cuts::pT > 30*GeV && Cuts::absrap < 4.4);
       if (jets.size() < 2) vetoEvent;
       const int njets = jets.size();
       const double htj = sum(jets, Kin::pT, 0.0);
@@ -79,7 +90,7 @@ namespace Rivet {
       const double m12 = dijet.mass();
       const double pt12 = dijet.pT();
       const double y12 = dijet.rap();
-      const double dy12 = abs(jets[0].rap() - jets[1].rap());
+      const double dy12 = std::abs(jets[0].rap() - jets[1].rap());
       const double dphi12 = deltaPhi(jets[0], jets[1]);
 
       // Get leading dijet + H
@@ -94,60 +105,65 @@ namespace Rivet {
         if (dy12 < DY12CUTS[i][0]) continue;
         if (dy12 >= DY12CUTS[i][1]) continue;
         if (dphijjH < DPHIJHCUTS[i]) continue;
-        _c_xs[i]->fill();
-        _h[i]["deltaphi_jj"]->fill(dphi12);
-        _h[i]["y_j12_02bin"]->fill(y12);
-        _h[i]["pth_largebin"]->fill(ptH/GeV);
-        _h[i]["pth_finebin"]->fill(ptH/GeV);
-        _h[i]["njets"]->fill(njets);
-        _h[i]["HT"]->fill(ht/GeV);
-        _h[i]["pthjj_largebin"]->fill(ptjjH/GeV);
-        _h[i]["ptjj_largebin"]->fill(pt12/GeV);
+        _c_xs[j][i]->fill();
+        _h[j][i]["deltaphi_jj"]->fill(dphi12);
+        _h[j][i]["y_j12_02bin"]->fill(y12);
+        _h[j][i]["pth_largebin"]->fill(ptH/GeV);
+        _h[j][i]["pth_finebin"]->fill(ptH/GeV);
+        _h[j][i]["njets"]->fill(njets);
+        _h[j][i]["HT"]->fill(ht/GeV);
+        _h[j][i]["pthjj_largebin"]->fill(ptjjH/GeV);
+        _h[j][i]["logpthjj_largebin"]->fill(log10(ptjjH/GeV));
+        _h[j][i]["ptjj_largebin"]->fill(pt12/GeV);
       }
 
       // Fill dijet mass histograms
-      _h_mjj["mjj_STXS"]->fill(m12/GeV);
-      _h_mjj["mjj_ATLAS_CONF_029"]->fill(m12/GeV);
-      _h_mjj["mjj_100GeVbin"]->fill(m12/GeV);
-      if (dy12 < 2)
-        _h_mjj["mjj_100GeVbin_center"]->fill(m12/GeV);
-      if (dy12 >= 2 && dy12 < 4)
-        _h_mjj["mjj_100GeVbin_middle"]->fill(m12/GeV);
-      if (dy12 > 4)
-        _h_mjj["mjj_100GeVbin_forward"]->fill(m12/GeV);
+      _h_mjj[j]["mjj_STXS"]->fill(m12/GeV);
+      _h_mjj[j]["mjj_ATLAS_CONF_029"]->fill(m12/GeV);
+      _h_mjj[j]["mjj_100GeVbin"]->fill(m12/GeV);
+      if (dy12 < 2.)
+        _h_mjj[j]["mjj_100GeVbin_center"]->fill(m12/GeV);
+      if (dy12 >= 2. && dy12 < 4.)
+        _h_mjj[j]["mjj_100GeVbin_middle"]->fill(m12/GeV);
+      if (dy12 > 4.)
+        _h_mjj[j]["mjj_100GeVbin_forward"]->fill(m12/GeV);
       if (dy12 > 3 && m12 > 400*GeV && dphijjH > 2.8)
-        _h_mjj["mjj_100GeVbin_ATLAS"]->fill(m12/GeV);
+        _h_mjj[j]["mjj_100GeVbin_ATLAS"]->fill(m12/GeV);
 
       // Now the dy12 histograms, with various m12 cuts
-      _h_dyjj["deltay_jj"]->fill(dy12);
+      _h_dyjj[j]["deltay_jj"]->fill(dy12);
       if (m12 < 350*GeV)
-        _h_dyjj["deltay_jj_light"]->fill(dy12);
+        _h_dyjj[j]["deltay_jj_light"]->fill(dy12);
       else // if (m12 < 350*GeV)
-        _h_dyjj["deltay_jj_heavy"]->fill(dy12);
+        _h_dyjj[j]["deltay_jj_heavy"]->fill(dy12);
+      }
     }
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
+      for (size_t j = 0; j < DRS.size(); ++j) {
       for (size_t i = 0; i < SELNAMES.size(); ++i) {
-        scale(_c_xs[i], crossSection()/femtobarn/sumOfWeights());
-        for (auto kv : _h[i])
+        scale(_c_xs[j][i], crossSection()/femtobarn/sumOfWeights());
+        for (auto kv : _h[j][i])
           scale(kv.second, crossSection()/femtobarn/sumOfWeights());
       }
-      for (auto kv : _h_mjj)
+      for (auto kv : _h_mjj[j])
         scale(kv.second, crossSection()/femtobarn/sumOfWeights());
-      for (auto kv : _h_dyjj)
+      for (auto kv : _h_dyjj[j])
         scale(kv.second, crossSection()/femtobarn/sumOfWeights());
+      }
     }
 
 
     /// @name Histograms
     //@{
-    map<string, Histo1DPtr> _h[8];
-    CounterPtr _c_xs[8];
-    map<string,Histo1DPtr> _h_mjj, _h_dyjj;
+    map<string, Histo1DPtr> _h[15][8];
+    CounterPtr _c_xs[15][8];
+    map<string,Histo1DPtr> _h_mjj[15], _h_dyjj[15];
     //@}
 
+    static const vector<double> DRS;
 
     /// Cut values for standard histogram sets (other than m12 and dy12)
     //@{
@@ -161,10 +177,10 @@ namespace Rivet {
 
 
   // Static const initializers
+  const vector<double>  MC_HJETSVBF::DRS = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5 };
   const vector<string>  MC_HJETSVBF::SELNAMES = {"", "_light_center", "_heavy_center", "_light_middle", "_heavy_middle", "_light_forward", "_heavy_forward", "_ATLAS"};
-  const vector<doubles> MC_HJETSVBF::M12CUTS = {{0., HUGE_VAL}, {0., 350.}, {350., HUGE_VAL}, {0., 350.}, {350., HUGE_VAL}, {0., 350.}, {350., HUGE_VAL}, {400., HUGE_VAL}};
-  // WRONG: const vector<doubles> MC_HJETSVBF::DY12CUTS = {{0., HUGE_VAL}, {0., 2.}, {2., 4.}, {4., HUGE_VAL}, {0., 2.}, {2., 4.}, {4., HUGE_VAL}, {3., HUGE_VAL}};
-  const vector<doubles> MC_HJETSVBF::DY12CUTS = {{0., HUGE_VAL}, {0., 2.}, {0., 2.}, {2., 4.}, {2., 4.}, {4., HUGE_VAL},  {4., HUGE_VAL}, {3., HUGE_VAL}};
+  const vector<doubles> MC_HJETSVBF::M12CUTS  = {{0., HUGE_VAL}, {0., 350.}, {350., HUGE_VAL}, {0., 350.}, {350., HUGE_VAL}, {0., 350.}, {350., HUGE_VAL}, {400., HUGE_VAL}};
+  const vector<doubles> MC_HJETSVBF::DY12CUTS = {{0., HUGE_VAL}, {0., 2.}, {0., 2.}, {2., 4.}, {2., 4.}, {4., HUGE_VAL}, {4., HUGE_VAL}, {3., HUGE_VAL}};
   const vector<double>  MC_HJETSVBF::DPHIJHCUTS = {0., 0., 0., 0., 0., 0., 0., 2.8};
 
 
